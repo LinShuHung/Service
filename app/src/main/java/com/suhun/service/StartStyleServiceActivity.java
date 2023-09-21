e55e1fbc64b4dc6a3d994c444c7ddeb1e50f33ed
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-09-21 11:50:09 +0800

    Add StartStyleServiceActivity layout

--- app/src/main/java/com/suhun/service/StartStyleServiceActivity.java
+++ app/src/main/java/com/suhun/service/StartStyleServiceActivity.java
@@ -2,7 +2,10 @@ package com.suhun.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartStyleServiceActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class StartStyleServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_style_service);
+    }
+
+    public void startStartStyleServiceFun(View view){
+    }
+
+    public void stopStartStyleServiceFun(View view){
+    }
+
+    public void finishStartStyleServiceFun(View view){
     }
 }
