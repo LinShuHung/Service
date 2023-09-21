008c8c2ddefbfd98a636140fa932dad777f5777a
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-09-21 11:13:25 +0800

    Add BindStyleServiceActivity layout

--- app/src/main/java/com/suhun/service/BindStyleServiceActivity.java
+++ app/src/main/java/com/suhun/service/BindStyleServiceActivity.java
@@ -3,6 +3,7 @@ package com.suhun.service;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class BindStyleServiceActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class BindStyleServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bind_style_service);
+    }
+
+    public void startBindServiceFun(View view){
+
+    }
+
+    public void stopBindServiceFun(View view){
+
+    }
+
+    public void getNameFun(View view){
+
+    }
+
+    public void finishBindFun(View view){
+        finish();
     }
 }
