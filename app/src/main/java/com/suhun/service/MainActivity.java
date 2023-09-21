8dabe754b417068a2d9a7614b04af75b502bcc00
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-09-21 10:56:56 +0800

    Implement Service Menu layout

--- app/src/main/java/com/suhun/service/MainActivity.java
+++ app/src/main/java/com/suhun/service/MainActivity.java
@@ -1,14 +1,35 @@
 package com.suhun.service;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+
+    ActivityResultLauncher resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goStartStyleService(View view){
+
+    }
+
+    public void goBindStyleService(View view){
+
     }
 }
