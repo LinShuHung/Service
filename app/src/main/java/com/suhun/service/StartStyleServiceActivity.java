c5d464b7c29df985416dd8ddf76404a4bae03a92
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-09-21 11:55:25 +0800

    Initial view

--- app/src/main/java/com/suhun/service/StartStyleServiceActivity.java
+++ app/src/main/java/com/suhun/service/StartStyleServiceActivity.java
@@ -6,13 +6,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class StartStyleServiceActivity extends AppCompatActivity {
+    private String tag = StartStyleServiceActivity.class.getSimpleName();
+    private TextView resultCounter;
+    private SeekBar counterSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_style_service);
+        initView();
+    }
+
+    private void initView(){
+        resultCounter = findViewById(R.id.lid_counterResult);
+        counterSeekBar = findViewById(R.id.lid_counterSeekBar);
     }
 
     public void startStartStyleServiceFun(View view){
